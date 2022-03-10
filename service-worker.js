0bd740879d72fc4d9e44d6be0e02b704668485a6
Mary Margaret <mmlawton15@gmail.com>
2022-03-10 10:37:40 -0500

    having a cache error

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const APP_PREFIX = 'FoodFest-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./events.html",
+    "./tickets.html",
+    "./schedule.html",
+    "./assets/css/style.css",
+    "./assets/css/bootstrap.css",
+    "./assets/css/tickets.css",
+    "./dist/app.bundle.js",
+    "./dist/events.bundle.js",
+    "./dist/tickets.bundle.js",
+    "./dist/schedule.bundle.js"
+];
+
+self.addEventListenet('install', function (e) {
+    e.waitUntil( //tells the browser to wait until the work is complete before terminating the service worker
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache: ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
